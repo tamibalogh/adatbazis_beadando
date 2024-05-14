ad1b44abd56f13e374cdd956393a8dce4d650a7f
hodver <152182818+hodver@users.noreply.github.com>
2024-05-15 00:19:05 +0200

    feltöltés

--- lekerdezesek.sql
+++ lekerdezesek.sql
@@ -1,9 +1,40 @@
---Hazai csapatok sorrendben kapott gólok és szerzett gólok különbsége alapján
-SELECT cs.NEV,
-	   SUM(KAPOTT_GOL)-SUM(SZERZETT_GOL) AS "KULONBSEG"
-FROM Merkozesek m JOIN Csapatok cs ON m.HAZAI_CSAPAT_ID=cs.ID
-GROUP BY cs.NEV
-ORDER BY SUM(KAPOTT_GOL)-SUM(SZERZETT_GOL)
+--Az adatbázis összes adata
+SELECT 
+    c.nev AS csapat_neve,
+    m.hazai_csapat_id,
+    m.vendeg_csapat_id,
+    m.szerzett_gol AS hazai_szerzett_gol,
+    m.kapott_gol AS hazai_kapott_gol,
+    (CASE WHEN m.hazai_csapat_id = c.id THEN 'hazai' ELSE 'vendég' END) AS helyszin,
+    m.fordulo_id
+FROM 
+    merkozesek m
+JOIN 
+    csapatok c ON m.hazai_csapat_id = c.id OR m.vendeg_csapat_id = c.id
+ORDER BY 
+    m.fordulo_id;
+
+--Csapatok sorrendben kapott gólok és szerzett gólok különbsége alapján
+SELECT 
+    csapat_nev,
+    SUM(osszes_szerzett_gol) AS osszes_szerzett_gol,
+    SUM(osszes_kapott_gol) AS osszes_kapott_gol,
+    SUM(golkulonbseg) AS osszes_golkulonbseg
+FROM (
+    SELECT 
+        CASE WHEN m.hazai_csapat_id = c.id THEN m.szerzett_gol ELSE m.kapott_gol END AS osszes_szerzett_gol,
+        CASE WHEN m.hazai_csapat_id = c.id THEN m.kapott_gol ELSE m.szerzett_gol END AS osszes_kapott_gol,
+        CASE WHEN m.hazai_csapat_id = c.id THEN m.szerzett_gol - m.kapott_gol ELSE m.kapott_gol - m.szerzett_gol END AS golkulonbseg,
+        c.nev AS csapat_nev
+    FROM 
+        csapatok c
+    JOIN 
+        merkozesek m ON c.id = m.hazai_csapat_id OR c.id = m.vendeg_csapat_id
+) AS sub
+GROUP BY 
+    csapat_nev
+ORDER BY 
+    osszes_golkulonbseg DESC;
 
 --Legnagyobb férőhelyű stadion csapatának elnöke
 SELECT TOP 1 cs.ELNOK
